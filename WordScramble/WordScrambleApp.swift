ef7f9e8508b46e9642c60d0b344fd46c50796a2b
enesozmus <ensozmus@gmail.com>
2024-03-07 12:21:11 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by enesozmus on 7.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
